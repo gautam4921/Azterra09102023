16a4ca8fa0d1178b46e0c7009b9b0ad287c5886a
gautam4921 <45917285+gautam4921@users.noreply.github.com>
2023-10-09 10:18:18 +0530

    Update variables.tf and provided the keys to build

--- variables.tf
+++ variables.tf
@@ -5,16 +5,16 @@ variable "location" {
 default = "East US"    
 }
 variable "spn-client-id" {
-default = "f04d30a2-4171-402a-b28a-ec39d3c07910"    
+default = "442c3a03-6b98-4704-bb6e-c719b6e533c6"    
 }
 variable "spn-client-secret" {
-default = "RSu7Q~dpS-HEZrY4qzfigM4cqkP7-6OJsFmau"    
+default = "ObI8Q~AqbF_L6Zb.B0RTa39kmTdba2aSEjuedaoH"    
 }
 variable "spn-tenant-id" {
-default = "ab03b6dd-3642-483a-ae79-cb68c359c116"    
+default = "62af9dfb-ef59-4700-aee9-03593def1bba"    
 }
 variable "subscription_id" {
-default = "b2ceda44-2b61-42fb-a4bc-3dae69cbab50"
+default = "90f4dd64-b8a4-40a8-898f-a777acc25b9a"
 } 
 
 variable "azurerm_windows_virtual_machine" {
